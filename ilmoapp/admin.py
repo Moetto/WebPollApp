a1e4ce1d5d902c30bde0678d1831074e9eded768
Antti Möttönen <antti.moettoenen@gmail.com>
2016-02-05 16:27:05 +0200

    Added more models with polymorphism

--- ilmoapp/admin.py
+++ ilmoapp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from ilmoapp.models import *
+from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModelAdmin
 
-# Register your models here.
+
+class TextQuestionAdmin(PolymorphicChildModelAdmin):
+    base_model = TextQuestion
+
+
+class SelectOneQuestionAdmin(PolymorphicChildModelAdmin):
+    base_model = SelectOneQuestion
+
+
+class QuestionAdmin(PolymorphicParentModelAdmin):
+    base_model = Question
+    child_models = (
+        (TextQuestion, TextQuestionAdmin),
+        (SelectOneQuestion, SelectOneQuestionAdmin)
+    )
+
+
+admin.site.register(QuestionForm)
+admin.site.register(TextQuestion, TextQuestionAdmin)
+admin.site.register(Question, QuestionAdmin)
--- ilmoapp/models.py
+++ ilmoapp/models.py
@@ -1,5 +1,25 @@
 from django.db import models
+from polymorphic.models import PolymorphicModel
 
 
-class Question(models.Model):
+class QuestionForm(models.Model):
+    title = models.CharField(max_length=100)
+    questions = models.ManyToManyField('Question')
+
+    def __str__(self):
+        return self.title
+
+
+class Question(PolymorphicModel):
     title = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class TextQuestion(Question):
+    answer = models.TextField(max_length=1000, blank=True, null=True)
+
+
+class SelectOneQuestion(Question):
+    answer = models.IntegerField(blank=True, null=True)
